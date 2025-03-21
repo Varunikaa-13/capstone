121cacd00b5be26e08d435d2ce9d21196facf757
VARUNIKAA K <varunikaak.22cse@kongu.edu>
2025-03-21 11:55:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u varunikaa1313 -p dckr_pat_QN-jVktfYQ1upu-4-G6dsOh8Z-68
+    docker tag test varunikaa1313/dev
+    docker push varunikaa1313/dev
     docker-compose up -d
     
